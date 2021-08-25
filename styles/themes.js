b4e8857210be365a66ad6d97c1b9025a6612f03b
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-08-25 17:19:20 -0400

    Create base light/dark modes

---
+++ styles/themes.js
@@ -0,0 +1,15 @@
+export const lightTheme = {
+  primaryBg: 'hsl(209, 33%, 96%)',
+  buttonBg: 'hsl(209, 91%, 45%)',
+  textColor: 'hsl(207, 43%, 16%)',
+  linkTextColor: 'hsl(209, 91%, 45%)',
+  buttonTextColor: 'hsl(209, 33%, 96%)',
+};
+
+export const darkTheme = {
+  primaryBg: 'hsl(207, 43%, 16%)',
+  buttonBg: 'hsl(43, 60%, 68%)',
+  textColor: 'hsl(209, 33%, 96%)',
+  linkTextColor: 'hsl(43, 60%, 68%)',
+  buttonTextColor: 'hsl(207, 43%, 16%)',
+};
