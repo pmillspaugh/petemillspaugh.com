750e0bd8fad8c7073da60df6df946b6f59815185
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-08-25 17:22:23 -0400

    Use ThemeProvider from styled-components to wrap top-level app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,27 @@
-import { CSSReset } from '../styles/globals';
+import { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from '../styles/themes';
+import { CSSReset, GlobalStyle } from '../styles/globals';
+import ThemeToggle from '../components/ThemeToggle';
 
-function App({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
+  const [lightMode, setLightMode] = useState(true);
+
   return (
-    <>
+    <ThemeProvider theme={lightMode ? lightTheme : darkTheme}>
+      <Wrapper>
+        <ThemeToggle lightMode={lightMode} setLightMode={setLightMode} />
+        <Component {...pageProps} />
+      </Wrapper>
       <CSSReset />
-      <Component {...pageProps} />
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   );
-}
+};
+
+const Wrapper = styled.div`
+  height: 100%;
+  background-color: ${(p) => p.theme.primaryBg};
+`;
 
 export default App;
