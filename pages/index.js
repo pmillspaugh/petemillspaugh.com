0a4182d41717b1f6b92dd293680c250c974f501c
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-08-31 09:52:02 -0400

    Responsively style mountain graphic on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import styled from 'styled-components';
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
+import mountains from '../public/images/mountainFooter.png';
 
-export default function Home() {
+const Home = () => {
   return (
-    <div>
+    <Wrapper>
       <Head>
         <title>Peter Millspaugh</title>
         <meta
@@ -12,33 +15,57 @@ export default function Home() {
         />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-
-      <h1>Home.</h1>
-      <div>
-        <Link href='/about'>
-          <a>About</a>
-        </Link>
-      </div>
-      <div>
-        <Link href='/work'>
-          <a>Work</a>
-        </Link>
-      </div>
-      <div>
-        <Link href='/talks'>
-          <a>Talks</a>
-        </Link>
-      </div>
-      <div>
-        <Link href='/blog'>
-          <a>Blog</a>
-        </Link>
-      </div>
-      <div>
-        <Link href='#'>
-          <a>Contact</a>
-        </Link>
-      </div>
-    </div>
+      <Link href='/about'>
+        <a>About</a>
+      </Link>
+      <Link href='/work'>
+        <a>Work</a>
+      </Link>
+      <Link href='/talks'>
+        <a>Talks</a>
+      </Link>
+      <Link href='#'>
+        <a>Contact</a>
+      </Link>
+      <ImageWrapper>
+        <Image
+          src={mountains}
+          alt='Graphic illustration of mountains and pine trees'
+        />
+      </ImageWrapper>
+    </Wrapper>
   );
-}
+};
+
+const Wrapper = styled.div`
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 60px;
+  overflow: hidden;
+`;
+
+const ImageWrapper = styled.div`
+  position: relative;
+  margin-top: auto;
+  height: 35vh;
+  min-width: calc((1920 / 492) * 35vh);
+  overflow: hidden;
+  line-height: 0;
+
+  @media (min-width: 1000px) {
+    height: 40vh;
+    min-width: calc((1920 / 492) * 40vh);
+  }
+  @media (min-width: 1200px) {
+    height: 45vh;
+    min-width: calc((1920 / 492) * 45vh);
+  }
+  @media (min-width: 1400px) {
+    height: 50vh;
+    min-width: calc((1920 / 492) * 50vh);
+  }
+`;
+
+export default Home;
