e89cabd852a1d0a8c73cfc3f8a1473752a6116bc
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-09-13 11:40:51 -0400

    Add contact info section and home link to About page

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,8 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 import mountainsIcon from '../public/images/mountainsIcon.png';
+import { Mail, Linkedin, GitHub } from 'react-feather';
+import HomeLink from '../components/HomeLink';
 import Spacer from '../components/utils/Spacer';
 import Divider from '../components/utils/Divider';
 
@@ -17,6 +19,7 @@ const About = () => {
         />
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <HomeLink />
       <Wrapper>
         <ContentWrapper>
           <Spacer size='24px' />
@@ -30,8 +33,8 @@ const About = () => {
           <Heading>About</Heading>
           <Spacer size='24px' />
           <Divider />
-          <Spacer size='24px' />
-          <Paragraph>Hi there 👋</Paragraph>
+          <Spacer size='36px' />
+          <SubHeading>Hi there 👋</SubHeading>
           <Spacer size='20px' />
           <Paragraph>Thanks for visiting my site!</Paragraph>
           <Spacer size='20px' />
@@ -41,8 +44,10 @@ const About = () => {
           </Paragraph>
           <Spacer size='20px' />
           <Paragraph>
-            I mostly focus on the frontend (React), and I&apos;m especially
-            excited about Accessibility and PWAs. In July I gave a{' '}
+            I enjoy working across the stack in the TypeScript/JavaScript
+            ecosystem (React, Redux, Node.js) with a core focus on frontend
+            technologies. I&apos;m especially excited about Accessibility and
+            PWAs. In July, I gave a{' '}
             <Link href='/talks' passHref>
               <AnchorLink>talk on Web Accessibility</AnchorLink>
             </Link>{' '}
@@ -67,8 +72,31 @@ const About = () => {
             When I&apos;m not coding, I do a lot of cooking (
             <em>read: pasta</em>) and running.
           </Paragraph>
+          <Spacer size='28px' />
+          <SubHeading id='contact'>Let&apos;s connect! ☕</SubHeading>
           <Spacer size='20px' />
-          <Paragraph>[Contact]</Paragraph>
+          <IconWrapper>
+            <IconLink
+              href='mailto:peterdgmillspaugh@gmail.com'
+              aria-label="Peter's email address"
+            >
+              <Mail size={24} />
+            </IconLink>
+            <Spacer size='20px' />
+            <IconLink
+              href='https://www.linkedin.com/in/peter-millspaugh/'
+              aria-label="Peter's LinkedIn profile"
+            >
+              <Linkedin size={24} />
+            </IconLink>
+            <Spacer size='20px' />
+            <IconLink
+              href='https://github.com/pmillspaugh'
+              aria-label="Peter's GitHub profile"
+            >
+              <GitHub size={24} />
+            </IconLink>
+          </IconWrapper>
         </ContentWrapper>
       </Wrapper>
     </>
@@ -115,6 +143,12 @@ const Heading = styled.h1`
   color: ${(p) => p.theme.headingColor};
 `;
 
+const SubHeading = styled.h2`
+  font-size: 2rem;
+  width: 100%;
+  color: ${(p) => p.theme.textColor};
+`;
+
 const Paragraph = styled.p`
   width: 100%;
   font-size: 1.25rem;
@@ -131,6 +165,26 @@ const AnchorLink = styled.a`
 
   &:hover {
     color: ${(p) => p.theme.linkTextColorHover};
+  }
+`;
+
+const IconWrapper = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+const IconLink = styled.a`
+  width: 48px;
+  height: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  background-color: ${(p) => p.theme.buttonBg};
+  color: ${(p) => p.theme.buttonTextColor};
+
+  &:hover {
+    background-color: ${(p) => p.theme.buttonBgHover};
   }
 `;
 
