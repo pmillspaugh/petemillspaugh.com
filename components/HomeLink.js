3be8587dc72a10fc7a664d683223b130626b40d4
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-09-13 11:40:08 -0400

    Create component to link to homepage from other pages

---
+++ components/HomeLink.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import Link from 'next/link';
+import { Home } from 'react-feather';
+
+const HomeLink = () => {
+  return (
+    <Link href='/' passHref>
+      <AnchorLink>
+        <Home />
+      </AnchorLink>
+    </Link>
+  );
+};
+
+const AnchorLink = styled.a`
+  position: absolute;
+  top: 12px;
+  left: 12px;
+  width: 48px;
+  height: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  border-radius: 50%;
+  color: ${(p) => p.theme.linkTextColor};
+
+  &:hover {
+    cursor: pointer;
+    background-color: ${(p) => p.theme.buttonBgHover};
+    color: ${(p) => p.theme.primaryBg};
+  }
+`;
+
+export default HomeLink;
