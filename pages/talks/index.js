830213ed75f1aa5ad966f29816c7edd8b53d586c
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-09-13 18:21:59 -0400

    Create talks page and add Accessibility talk

--- pages/talks/index.js
+++ pages/talks/index.js
@@ -1,5 +1,87 @@
+import styled from 'styled-components';
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+import accessibilityThumbnail from '../../public/images/accessibilityThumbnail.png';
+import HomeLink from '../../components/HomeLink';
+import Layout from '../../components/utils/Layout';
+import MaxWidthWrapper from '../../components/utils/MaxWidthWrapper';
+import PageHeader from '../../components/utils/PageHeader';
+import AnchorLink from '../../components/utils/AnchorLink';
+import Spacer from '../../components/utils/Spacer';
+
 const Talks = () => {
-  return <h1>Talks.</h1>;
+  return (
+    <>
+      <Head>
+        <title>Peter Millspaugh | Talks</title>
+        <meta name='description' content='Peter Millspaugh - Talks' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <HomeLink />
+      <Layout>
+        <MaxWidthWrapper>
+          <PageHeader page='Talks' />
+          <Spacer size='8px' />
+          <TalkWrapper>
+            <TalkBlurb>
+              <ThumbnailandHeadingWrapper>
+                <Link href='/talks/accessibility' passHref>
+                  <TalkLink>Web Accessibility for Frontend Developers</TalkLink>
+                </Link>
+                <Spacer size='24px' />
+                <ThumbnailWrapper>
+                  <Thumbnail
+                    src={accessibilityThumbnail}
+                    alt="Cover slide of Peter's talk on Web Accessibility"
+                  />
+                </ThumbnailWrapper>
+              </ThumbnailandHeadingWrapper>
+            </TalkBlurb>
+          </TalkWrapper>
+        </MaxWidthWrapper>
+      </Layout>
+    </>
+  );
 };
+
+const TalkWrapper = styled.main`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const TalkBlurb = styled.article`
+  flex-basis: 100%;
+  max-width: 800px;
+  padding: 36px;
+  background-color: ${(p) => p.theme.secondaryBg};
+  border: 1px solid ${(p) => p.theme.borderColor};
+  border-radius: 8px;
+`;
+
+const TalkLink = styled(AnchorLink)`
+  font-size: 1.5rem;
+  font-weight: 800;
+  font-family: 'Ubuntu', sans-serif;
+`;
+
+const ThumbnailandHeadingWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 992px) {
+    flex-direction: row;
+  }
+`;
+
+const ThumbnailWrapper = styled.div`
+  max-width: 300px;
+  line-height: 0;
+`;
+
+const Thumbnail = styled(Image)`
+  border-radius: 8px;
+`;
 
 export default Talks;
