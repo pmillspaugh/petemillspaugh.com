984d887fc0fe978c6a2481f18dbd6d2f94aaec52
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-09-14 17:03:42 -0400

    Specify lang attribute in html doc

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class Doc extends Document {
   render() {
     return (
-      <Html>
+      <Html lang='en'>
         <Head />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
