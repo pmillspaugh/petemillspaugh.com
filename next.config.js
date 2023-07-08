2676349a49cff04a600e3c8aa3259a8e5528fb09
Peter Millspaugh <peter@seedfi.com>
2023-07-08 10:09:25 -0400

    Switch from babel to swc

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-}
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options
+    styledComponents: true,
+  },
+};
