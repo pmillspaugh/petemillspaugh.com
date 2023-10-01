14277a38c9548cd6c4a3b12362cbae1b363c7744
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2023-10-01 10:23:43 -0400

    Move tag styles to Garden

--- src/components/Post/Tag.tsx
+++ src/components/Post/Tag.tsx
@@ -1,9 +1,10 @@
-import styled from "styled-components";
 import Tooltip from "@/components/Tooltip";
 import {
   PostFormat,
   PostFormatDescription,
   PostStatus,
+  OuterWrapper,
+  InnerWrapper,
 } from "@/components/Garden";
 import { LABEL_TO_DESCRIPTION } from "./Description";
 
@@ -22,28 +23,5 @@ const Tag = ({ label }: { label: PostFormat | PostStatus }) => {
     </Tooltip>
   );
 };
-
-const OuterWrapper = styled.div`
-  border-radius: 4px;
-  border: 1px solid var(--black);
-  padding: 3px;
-  background-color: ${(p) => p.theme.tagBorderColor};
-  transition: background-color var(--base-timing) ease-in-out;
-
-  &:hover {
-    background-color: ${(p) => p.theme.tagBorderColorHover};
-  }
-`;
-
-const InnerWrapper = styled.div`
-  padding: 2px 8px;
-  border-radius: 4px;
-  border: 1px solid var(--black);
-  background-color: ${(p) => p.theme.primaryBg};
-  transition: background-color var(--base-timing) ease-in-out;
-  font-family: var(--font-open-sans);
-  font-size: 0.875rem;
-  font-weight: 700;
-`;
 
 export default Tag;
