2b9d03a933710f39bd7a145b7d788441560a3203
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2023-10-22 11:14:43 -0400

    Update comments

--- src/pagefind/pagefind.js
+++ src/pagefind/pagefind.js
@@ -1,5 +1,7 @@
-// TODO: it would be nice to actually mock some result values here
+// This file gets overwritten by the build process
+// The mock response is to silence errors in development
 
 export const search = () => {
+  // TODO: it would be nice to actually mock some result values here
   return { results: [] };
 };
