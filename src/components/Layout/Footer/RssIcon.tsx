599205a4fed348399c613b5e912828d0a7eda821
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2023-11-04 16:58:45 -0400

    Add RSS icon to footer

---
+++ src/components/Layout/Footer/RssIcon.tsx
@@ -0,0 +1,36 @@
+const RssIcon = () => {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 13.625C7.28205 13.625 10.4297 14.9288 12.7504 17.2496C15.0712 19.5703 16.375 22.7179 16.375 26"
+        stroke="currentColor"
+        strokeWidth="1.75"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4 4C9.83476 4 15.4305 6.31785 19.5563 10.4437C23.6821 14.5695 26 20.1652 26 26"
+        stroke="currentColor"
+        strokeWidth="1.75"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5.375 26C6.13439 26 6.75 25.3844 6.75 24.625C6.75 23.8656 6.13439 23.25 5.375 23.25C4.61561 23.25 4 23.8656 4 24.625C4 25.3844 4.61561 26 5.375 26Z"
+        fill="currentColor"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default RssIcon;
