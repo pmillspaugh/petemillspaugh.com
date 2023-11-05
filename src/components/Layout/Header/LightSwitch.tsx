fb5c8e3d7efc7685ddb115919173a70de7e706d0
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2023-11-05 17:47:39 -0500

    Enable dragging by touch on mobile

--- src/components/Layout/Header/LightSwitch.tsx
+++ src/components/Layout/Header/LightSwitch.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useCallback } from "react";
 import { useSpring, animated } from "@react-spring/web";
 import VisuallyHidden from "@/components/VisuallyHidden";
 import styled from "styled-components";
@@ -14,16 +14,21 @@ const LightSwitch = ({ lightMode, setLightMode }: LightSwitchProps) => {
   const RANGE = 32;
 
   const [dragging, setDragging] = useState(false);
-  const [mouseDownY, setMouseDownY] = useState(0);
+  const [initialY, setInitialY] = useState(0);
   const [currentY, setCurrentY] = useState(0);
   const [clicked, setClicked] = useState(false);
 
   const audioRef = useRef<HTMLAudioElement>(null);
+  const playAudio = useCallback(() => {
+    if (JSON.parse(localStorage.getItem(LocalStorageKey.AudioEnabled))) {
+      audioRef.current?.play();
+    }
+  }, []);
 
   const springProps = useSpring({
     transform: clicked
       ? `translateY(${BOUNCE}px)`
-      : `translateY(${currentY - mouseDownY + OFFSET}px)`,
+      : `translateY(${currentY - initialY + OFFSET}px)`,
     config: {
       tension: 300,
       friction: 20,
@@ -32,35 +37,70 @@ const LightSwitch = ({ lightMode, setLightMode }: LightSwitchProps) => {
 
   const handleMouseDown = (event: React.MouseEvent<HTMLButtonElement>) => {
     setDragging(true);
-    setMouseDownY(event.clientY);
+    setInitialY(event.clientY);
     setCurrentY(event.clientY);
-    document.addEventListener("mouseup", handleMouseUp);
   };
 
   const handleMouseMove = (event: React.MouseEvent<HTMLButtonElement>) => {
     if (dragging) {
-      event.clientY < mouseDownY
-        ? setCurrentY(mouseDownY)
-        : setCurrentY(Math.min(event.clientY, mouseDownY + RANGE));
+      event.clientY < initialY
+        ? setCurrentY(initialY)
+        : setCurrentY(Math.min(event.clientY, initialY + RANGE));
+    }
+  };
+
+  const handleMouseOut = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (dragging) {
+      document.addEventListener("mouseup", handleMouseUp);
     }
   };
 
   const handleMouseUp = () => {
     setLightMode(!lightMode);
     setDragging(false);
-    document.removeEventListener("mouseup", handleMouseUp);
+    playAudio();
 
-    if (currentY === mouseDownY && currentY !== 0) {
+    if (currentY === initialY && currentY !== 0) {
       setClicked(true);
       setTimeout(() => setClicked(false), 250);
     } else {
       setCurrentY(0);
-      setMouseDownY(0);
+      setInitialY(0);
     }
 
-    if (JSON.parse(localStorage.getItem(LocalStorageKey.AudioEnabled))) {
-      audioRef.current?.play();
+    document.removeEventListener("mouseup", handleMouseUp);
+  };
+
+  const handleTouchStart = (event: React.TouchEvent<HTMLButtonElement>) => {
+    // Prevent pull-to-refresh behavior on mobile
+    document.documentElement.style.overscrollBehavior = "none";
+    document.addEventListener("touchend", handleTouchEnd);
+
+    setDragging(true);
+    setInitialY(event.touches[0].clientY);
+    setCurrentY(event.touches[0].clientY);
+  };
+
+  const handleTouchMove = (event: React.TouchEvent<HTMLButtonElement>) => {
+    if (dragging) {
+      event.touches[0].clientY < initialY
+        ? setCurrentY(initialY)
+        : setCurrentY(Math.min(event.touches[0].clientY, initialY + RANGE));
     }
+  };
+
+  const handleTouchEnd = () => {
+    if (currentY !== initialY) {
+      setLightMode(!lightMode);
+      setDragging(false);
+      setCurrentY(0);
+      setInitialY(0);
+      playAudio();
+    }
+
+    // Re-enable pull-to-refresh behavior on mobile
+    document.documentElement.style.overscrollBehavior = "auto";
+    document.removeEventListener("touchend", handleTouchEnd);
   };
 
   const handleKeyUp = (event: React.KeyboardEvent<HTMLButtonElement>) => {
@@ -76,6 +116,10 @@ const LightSwitch = ({ lightMode, setLightMode }: LightSwitchProps) => {
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
+      onMouseOut={handleMouseOut}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
       onKeyUp={handleKeyUp}
       aria-label="Theme Toggle"
       style={springProps}
@@ -98,6 +142,17 @@ const StyledButton = styled(animated.button)`
 
   &:hover {
     cursor: grab;
+  }
+
+  /* Enlarges target element (easier to click) */
+  &::before {
+    content: "";
+    position: absolute;
+    z-index: 0;
+    top: 0px;
+    left: -16px;
+    right: -16px;
+    bottom: -16px;
   }
 `;
 
