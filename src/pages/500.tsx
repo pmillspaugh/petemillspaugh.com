ee47d0f90fd56e02ff859dbcc43023b98d00b7eb
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2023-11-19 09:59:48 -0500

    Add head to 404 and 500 pages

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,18 +1,24 @@
-import Link from "next/link";
+import Head from "next/head";
 import styled from "styled-components";
 
 const ServerErrorPage = () => {
   return (
-    <StyledWrapper data-pagefind-ignore>
-      <h1>500</h1>
-      <p>
-        Gardener error.{" "}
-        <a href="https://twitter.com/pete_millspaugh" target="_blank">
-          Lmk
-        </a>{" "}
-        if my plants need watering, so to speak.
-      </p>
-    </StyledWrapper>
+    <>
+      <Head>
+        <title>Pete Millspaugh | Gardener error</title>
+        <meta name="description" content="Pete Millspaugh - Gardener error" />
+      </Head>
+      <StyledWrapper data-pagefind-ignore>
+        <h1>500</h1>
+        <p>
+          Gardener error.{" "}
+          <a href="https://twitter.com/pete_millspaugh" target="_blank">
+            Lmk
+          </a>{" "}
+          if my plants need watering, so to speak.
+        </p>
+      </StyledWrapper>
+    </>
   );
 };
 
@@ -26,6 +32,13 @@ const StyledWrapper = styled.div`
   & > h1 {
     font-size: 4rem;
     font-weight: 900;
+  }
+
+  @media (min-width: 1200px) {
+    & > p {
+      font-size: 1.25rem;
+      line-height: 1.75;
+    }
   }
 `;
 
