d4ac2faea716a37cbd1308910c5ff49c1f0f957c
Pete Millspaugh <peterdgmillspaugh@gmail.com>
2025-02-14 19:50:02 -0500

    Update width of Divider

--- src/components/Post/Divider.tsx
+++ src/components/Post/Divider.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Divider = styled.div`
   height: 6px;
+  width: 112px;
   margin-top: 40px;
   border-top: 1.5px solid ${(p) => p.theme.textColor};
   border-bottom: 1.5px solid ${(p) => p.theme.textColor};
