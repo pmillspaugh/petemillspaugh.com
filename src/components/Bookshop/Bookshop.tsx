aee572f48f30921568893aadc76ebcb589b2909b
Pete Millspaugh <peterdgmillspaugh@gmail.com>
2025-10-03 10:26:19 -0400

    September 2025

--- src/components/Bookshop/Bookshop.tsx
+++ src/components/Bookshop/Bookshop.tsx
@@ -6,7 +6,7 @@ export default function Bookshop() {
       Btw, I’m an affiliate of <a href="https://bookshop.org">bookshop.org</a>,
       so if you buy a book I link to I’ll make a 10% commision on the profit.
       Even if you don’t click my links, I recommend Bookshop.org over Amazon to
-      support independent bookstores!
+      support independent bookstores.
     </Callout>
   );
 }
