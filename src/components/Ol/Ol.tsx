7ecb8f0c670f34b91b1cf80e703fca0d303ca48a
David Eisinger <david.eisinger@gmail.com>
2026-09-19 00:46:46 -0400

    Use GFM for footnotes (+ fix backlinks)

--- src/components/Ol/Ol.tsx
+++ src/components/Ol/Ol.tsx
@@ -14,6 +14,36 @@ const StyledOl = styled.ul`
     padding-left: 8px;
   }
 
+  [data-footnotes] > & {
+    padding-left: 0;
+    counter-reset: footnote;
+
+    & > li {
+      list-style-type: none;
+      padding-left: 0;
+      counter-increment: footnote;
+    }
+
+    & > li > p:first-child::before {
+      content: "[" counter(footnote) "]";
+      display: inline-block;
+      transform: translateY(-1px);
+      font-family: var(--font-mono), monospace;
+      font-size: 0.75rem;
+      font-weight: 600;
+      padding: 2px 4px 1px;
+      margin-right: 0.25em;
+      border-radius: 4px;
+      background-color: ${(p) => p.theme.codeBg};
+      color: ${(p) => p.theme.codeColor};
+      transition: all var(--base-timing) ease-in-out;
+
+      @media (min-width: 1200px) {
+        font-size: 0.9rem;
+      }
+    }
+  }
+
   @media (min-width: 1200px) {
     font-size: 1.25rem;
     margin-bottom: 24px;
